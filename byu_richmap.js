56207addd2f474a38c6b4f84b030557405842b07
homerjensen <homerjensen@github.com>
2018-02-01 11:20:20 -0700

    commit1.0
    commit1.0

---
+++ byu_richmap.js
@@ -0,0 +1,84 @@
+
+/////////////////WSC//////////////////////////
+
+//stores marker as variable
+var wsc = document.getElementById("wsc");
+var _wsc = document.getElementById("_wsc")
+//stores event function
+var onBuildingClick = function() {
+	document.getElementById("_wsc").classList.remove("_card");
+	document.getElementById("_wsc").classList.add("card");
+	document.getElementById("_wsc").innerHTML = "This is the Wilk!";
+	//img functionality
+	var wscimgform = document.createElement("form");
+	var wscimginput = document.createElement("input");
+	wscimgform.appendChild(wscimginput);
+	_wsc.appendChild(wscimgform);
+	wscimginput.setAttribute("id", "wscimginput");
+	wscimginput.setAttribute("type", "file");
+	wscimginput.setAttribute("accept", "image/*");
+	var wscimg = document.createElement("img");
+	_wsc.appendChild(wscimg);
+	wscimg.setAttribute("src", "wscimginput");
+	wscimg.setAttribute("position", "absolute");
+	wscimg.setAttribute("height", "80%");
+	wscimg.setAttribute("top", "20%");
+}
+//adds event listener to marker
+wsc.addEventListener("click", onBuildingClick);
+//stores compass as a variable
+var compass = document.getElementById("compass_img");
+//stores event function
+var onCompassClick = function() {
+	document.getElementById("_wsc").classList.remove("card");
+	document.getElementById("_wsc").classList.add("_card");
+	document.getElementById("_wsc").innerHTML = "";
+}
+//adds event listener to compass
+compass.addEventListener("click", onCompassClick);
+
+//////////////////////////////HBLL//////////////////////
+
+//stores marker as variable
+var hbll = document.getElementById("hbll");
+//stores event function
+var onBuildingClick = function() {
+	document.getElementById("_hbll").classList.remove("_card");
+	document.getElementById("_hbll").classList.add("card");
+	document.getElementById("_hbll").innerHTML = "This is the Library!";
+}
+//adds event listener to marker
+hbll.addEventListener("click", onBuildingClick);
+//stores compass as a variable
+var compass = document.getElementById("compass_img");
+//stores event function
+var onCompassClick = function() {
+	document.getElementById("_hbll").classList.remove("card");
+	document.getElementById("_hbll").classList.add("_card");
+	document.getElementById("_hbll").innerHTML = "";
+}
+//adds event listener to compass
+compass.addEventListener("click", onCompassClick);
+
+//////////////////////////////HFAC//////////////////////
+
+//stores marker as variable
+var hfac = document.getElementById("hfac");
+//stores event function
+var onBuildingClick = function() {
+	document.getElementById("_hfac").classList.remove("_card");
+	document.getElementById("_hfac").classList.add("card");
+	document.getElementById("_hfac").innerHTML = "This is the HFAC!";
+}
+//adds event listener to marker
+hfac.addEventListener("click", onBuildingClick);
+//stores compass as a variable
+var compass = document.getElementById("compass_img");
+//stores event function
+var onCompassClick = function() {
+	document.getElementById("_hfac").classList.remove("card");
+	document.getElementById("_hfac").classList.add("_card");
+	document.getElementById("_hfac").innerHTML = "";
+}
+//adds event listener to compass
+compass.addEventListener("click", onCompassClick);
